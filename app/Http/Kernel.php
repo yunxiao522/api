3079b90f2c6af70415d1d3ea549c6ae11391b70a
ww <sucaiz@q.com>
2019-03-23 21:23:57 +0800

    1

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,7 +62,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'checkApi' => ApiAuth::class
+        'checkApi' => \App\Http\Middleware\ApiAuth::class
     ];
 
     /**
