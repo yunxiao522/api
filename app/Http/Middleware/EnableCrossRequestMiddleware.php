a70a8926692ce792ce6d7c1a21ab9ee99924eaaa
ww <sucaiz@q.com>
2019-03-26 14:49:25 +0800

    alter enableCrossRequest middleware

--- app/Http/Middleware/EnableCrossRequestMiddleware.php
+++ app/Http/Middleware/EnableCrossRequestMiddleware.php
@@ -17,6 +17,7 @@ class EnableCrossRequestMiddleware
     {
         $response = $next($request);
         $response->header('Access-Control-Allow-Origin', '*');
+        $response->header('Access-Control-Allow-Origin', 'https://www.sucai.biz');
         $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept');
         $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
         $response->header('Access-Control-Allow-Credentials', 'true');  //ession共享的需求才用到
