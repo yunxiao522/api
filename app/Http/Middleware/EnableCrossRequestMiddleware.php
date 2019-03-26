9e8b7a91c09c10005223871599e79f74ebbca328
ww <sucaiz@q.com>
2019-03-26 15:34:18 +0800

    alter enableCrossRequest middleware

--- app/Http/Middleware/EnableCrossRequestMiddleware.php
+++ app/Http/Middleware/EnableCrossRequestMiddleware.php
@@ -17,7 +17,6 @@ class EnableCrossRequestMiddleware
     {
         $response = $next($request);
         $response->header('Access-Control-Allow-Origin', '*');
-        $response->header('Access-Control-Allow-Origin', 'https://www.sucai.biz');
         $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept');
         $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
         return $response;
