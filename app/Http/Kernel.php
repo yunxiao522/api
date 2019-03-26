084957447f0766d1c507677fde0b49797172da1e
ww <sucaiz@q.com>
2019-03-26 14:41:50 +0800

    add cros request middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,6 +21,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
+        \App\Http\Middleware\EnableCrossRequestMiddleware::class,
     ];
 
     /**
@@ -62,7 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'apiauth' => \App\Http\Middleware\ApiAuth::class
+        'apiauth' => \App\Http\Middleware\ApiAuth::class,
     ];
 
     /**
