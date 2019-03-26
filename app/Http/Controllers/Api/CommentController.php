3eee54453afd3b1d4c4e9d99ea0bf9b029699a60
ww <sucaiz@q.com>
2019-03-26 12:26:29 +0800

    rename comment as commentcontroller

---
+++ app/Http/Controllers/Api/CommentController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yunxi
+ * Date: 2019/3/26 0026
+ * Time: 11:53
+ */
+
+namespace App\Http\Controllers\Api;
+
+
+use Illuminate\Http\Request;
+
+class CommentController extends BaseController
+{
+    public function __construct(Request $request)
+    {
+        parent::__construct($request);
+    }
+
+    public function getList(){
+        $id = $this->request->route('id');
+        $page = $this->request->route('page');
+        $limit = $this->request->route('limit');
+
+    }
+}
