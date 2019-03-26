beefdda8d17edf63bdd8eea3c55759bc49feee77
ww <sucaiz@q.com>
2019-03-26 15:51:38 +0800

    alter enableCrossRequest middleware

--- app/Http/Middleware/EnableCrossRequestMiddleware.php
+++ app/Http/Middleware/EnableCrossRequestMiddleware.php
@@ -15,10 +15,11 @@ class EnableCrossRequestMiddleware
      */
     public function handle($request, Closure $next)
     {
-        $response = $next($request);
-        $response->header('Access-Control-Allow-Origin', '*');
-        $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept');
-        $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
-        return $response;
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Credentials: true");
+        header("Access-Control-Allow-Methods: *");
+        header("Access-Control-Allow-Headers: Content-Type,Access-Token");
+        header("Access-Control-Expose-Headers: *");
+        return $next[$request];
     }
 }
