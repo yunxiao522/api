ae4d77d27ef0677ec12cbda4311ace88ea8e4fec
ww <sucaiz@q.com>
2019-03-27 14:42:50 +0800

    add route /feedback/push

--- routes/api.php
+++ routes/api.php
@@ -27,6 +27,8 @@ Route::prefix('/v1')->group(function(){
     Route::get('/column/getSonList/{id}','Api\ColumnController@getSonList')->where(['id'=>'[0-9]+']);
     //获取文档信息
     Route::get('/article/getInfo/{id}','Api\ArticleController@getInfo')->where(['id'=>'[0-9]+']);
+    //意见反馈
+    Route::post('/FeedBack/push/{uid?}','Api\FeedBackController@push');
 });
 //获取验证令牌
 Route::any('/getToken','Api\Auth@getToken');
