cd4b68f3a49fa98de9c82c5c4856ae1eeefe4aa8
ww <sucaiz@q.com>
2019-03-27 14:43:16 +0800

    add feedback controller

---
+++ app/Http/Controllers/Api/FeedBackController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yunxi
+ * Date: 2019/3/27 0027
+ * Time: 14:38
+ */
+
+namespace App\Http\Controllers\Api;
+
+
+use Illuminate\Http\Request;
+
+class FeedBackController extends BaseController
+{
+    public function __construct(Request $request)
+    {
+        parent::__construct($request);
+    }
+}
