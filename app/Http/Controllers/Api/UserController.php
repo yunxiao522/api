6e316e95e903fd0252ef2481340236068b38facd
ww <sucaiz@q.com>
2019-03-28 17:02:56 +0800

    add api edituser

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -40,11 +40,63 @@ class UserController extends BaseController
         return $face_url;
     }
 
-    //获取会员账号信息
+    /**
+     * Description 获取会员账号信息方法
+     */
     public function getUserInfo(){
         $uid = Auth::getUserId();
         $user_info = User::getOne(['id'=>$uid],['nickname','face','level','email','qq','gold','experience','sex','description','signature','create_time']);
         $user_info->create_time = date('Y-m-d',$user_info->create_time);
         Response::success($user_info);
     }
+
+    /**
+     *Description 修改会员账号信息方法
+     */
+    public function editUserInfo(){
+        $data = $this->checkForm();
+        if(is_string($data)){
+            Response::fail($data);
+        };
+        $uid = Auth::getUserId();
+        $res = User::edit(['id'=>$uid],$data);
+        if($res){
+            Response::success('修改成功');
+        }else{
+            Response::fail('修改失败');
+        }
+    }
+
+    /**
+     * @return string
+     * Description 验证修改账号信息表单
+     */
+    private function checkForm(){
+        $nickname = request('nickname');
+        if(!empty($nickname)){
+            $data['nickname'] = $nickname;
+            if(mb_strlen($nickname,'UTF-8')>20){
+                return '昵称不能超过20个字符';
+            }
+        }
+        $sex = request('sex');
+        if(!empty($sex)){
+            if(in_array($sex,User::$user_sex)){
+                $data['sex'] = array_search($sex,User::$user_sex);
+            }else{
+                return '性别不正确';
+            }
+        }
+        $qq = request('qq');
+        if(!empty($qq)){
+            if(is_numeric($qq) && mb_strlen($qq ,'UTF-8') <= 12){
+                $data['qq'] = $qq;
+            }else{
+                return 'qq账号格式不正确';
+            }
+        }
+        $data['alter_time'] = time();
+        return $data;
+    }
+
 }
