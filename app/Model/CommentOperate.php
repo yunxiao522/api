3cae61f749110b7e10293b104c24b4820b134768
ww <sucaiz@q.com>
2019-04-01 19:51:12 +0800

    add api comment praiser and comment oppose

--- app/Model/CommentOperate.php
+++ app/Model/CommentOperate.php
@@ -11,4 +11,9 @@ class CommentOperate extends Base
     {
         parent::__construct($attributes);
     }
+
+    //重构新增评论操作方法
+    public static function add($data){
+        return self::insert($data);
+    }
 }
