3ec4857d07a8a4e3f0e26385840e9d9c89713fa7
ww <sucaiz@q.com>
2019-04-02 18:04:53 +0800

    alter feedback controller function push success title

--- app/Model/Base.php
+++ app/Model/Base.php
@@ -35,7 +35,7 @@ class Base extends Model
      */
     public static function getOne($where, $field = '*' ,$order = ['id','desc'])
     {
-        return self::where($where)->orderBy($order[0],$order[1])->first($field)->cache();
+        return self::cache()->where($where)->orderBy($order[0],$order[1])->first($field);
     }
 
 
