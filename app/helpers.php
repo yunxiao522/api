e90c4784d4edd70c142dd27949c323a3bef721bb
ww <sucaiz@q.com>
2019-04-02 10:36:48 +0800

    add api get my comment

--- app/helpers.php
+++ app/helpers.php
@@ -1,6 +1,6 @@
 <?php
 //自定义助手函数定义文件
-if(function_exists('cut_str')){
+if(!function_exists('cut_str')){
     //截取字符串
     function cut_str($str , $length){
         if(mb_strlen($str,'UTF-8') > $length){
