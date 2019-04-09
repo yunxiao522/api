362ab73da07103fe13065ae864f7be4ccfc07722
ww <sucaiz@q.com>
2019-04-09 15:56:42 +0800

    add api mobile/getMobileTag

--- app/Http/Controllers/Api/MobileController.php
+++ app/Http/Controllers/Api/MobileController.php
@@ -4,6 +4,7 @@
 namespace App\Http\Controllers\Api;
 
 
+use App\Model\MobileTag;
 use Illuminate\Http\Request;
 
 class MobileController extends BaseController
@@ -13,8 +14,15 @@ class MobileController extends BaseController
         parent::__construct($request);
     }
 
-
+    /**
+     * Description 获取栏目tag列表数据
+     */
     public function getMobileTag(){
         $column_id = request('column_id');
+        if(empty($column_id) || !is_numeric($column_id)){
+            Response::fail('参数错误');
+        }
+        $list = MobileTag::getALL(['column_id'=>$column_id]);
+        Response::success($list,'','get data success');
     }
 }
