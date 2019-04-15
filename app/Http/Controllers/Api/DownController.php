373480efa3d1374baddee0c8111391fa2003ede7
ww <sucaiz@q.com>
2019-04-15 18:43:44 +0800

    alter getMyDown article text number

--- app/Http/Controllers/Api/DownController.php
+++ app/Http/Controllers/Api/DownController.php
@@ -28,7 +28,7 @@ class DownController extends BaseController
                 $list['data'][$key]['file_url'] = 'http://image.sucai.biz/2019-02-21/a81d6543f0f9700ad0534189cb3de34a.png';
             }
             $list['data'][$key]['column'] = Column::getField(['id'=>$value['column_id']],'type_name');
-            $list['data'][$key]['article'] = Article::getField(['id'=>$value['article_id']],'title');
+            $list['data'][$key]['article'] = self::cut_str(Article::getField(['id'=>$value['article_id']],'title'),10);
         }
         Response::success($list,'','获取数据成功');
     }
