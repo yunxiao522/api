0fbb05c0f69b76461c4f25e7c94e0fb6de72fd0b
ww <sucaiz@q.com>
2019-04-22 12:00:33 +0800

    add api /dowb/getDownUrl

--- app/Model/MyDown.php
+++ app/Model/MyDown.php
@@ -7,7 +7,7 @@ namespace App\Model;
 class MyDown extends Base
 {
     public $table = 'my_down';
-    public static $pk = '';
+    public static $pk = 'uid';
     public function __construct(array $attributes = [])
     {
         parent::__construct($attributes);
